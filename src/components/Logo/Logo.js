86e2595a59bbcdabf8e5206790c153a85abd4f71
darklordace <firdaus.alghifari@gmail.com>
2019-01-23 23:01:33 +0700

    First Commit

---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Tilt from 'react-tilt';
+import logo from '../../logo.png';
+import './Logo.css';
+
+const Logo = () => {
+    return (
+        <div className="outerTilt">
+            <Tilt className="Tilt br2 shadow-2" options={{ max : 55 }}>
+                <img
+                    src={logo}
+                    className="App-logo"
+                    alt="logo"
+                />
+            </Tilt>
+        </div>
+    );
+}
+
+export default Logo;
---
+++ src/containers/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Logo from '../components/Logo/Logo';
+import 'tachyons';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Logo />
+      </div>
+    );
+  }
+}
+
+export default App;
