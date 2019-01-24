9bd1cc58cbac4707e6ff5cd17c3a79eaa3ce96a4
darklordace <firdaus.alghifari@gmail.com>
2019-01-24 08:57:13 +0700

    stylings and general layout provided

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,12 +1,21 @@
 import React, { Component } from 'react';
 import Logo from '../components/Logo/Logo';
+
 import 'tachyons';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Logo />
+        <div className="mx-auto pt5 contents">
+          <h1>Zomato Easy Search</h1> {/* TITLE */}
+          {/* Search Bar */}
+          {/* Restaurant List */}
+        </div>
       </div>
     );
   }
